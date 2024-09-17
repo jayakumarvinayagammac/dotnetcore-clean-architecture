ea2dbb119f701c3b7c10ee8e752001a839408390
jayakumarvinayagammac <jayakumarvinayagammac@outlook.com>
2024-09-18 00:15:07 +0530

    Jay# Configuration updated

--- cqrs-and-mediatr/ClassicModelSource/src/ClassicModelInfrastructure/DataContexts/ClassicmodelsContext.cs
+++ cqrs-and-mediatr/ClassicModelSource/src/ClassicModelInfrastructure/DataContexts/ClassicmodelsContext.cs
@@ -32,10 +32,6 @@ public partial class ClassicModelsContext : DbContext
 
     public virtual DbSet<Productline> Productlines { get; set; }
 
-//     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-// #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-//         => optionsBuilder.UseMySQL("server=localhost;database=classicmodels;user=my_user;password=AqzLpm*67890");
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Customer>(entity =>
